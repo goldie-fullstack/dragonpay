029a5d9ff789808fb6c80415c2cc4407271fb238
Veronica <veronica@fullstack.ph>
2024-06-25 15:13:55 +0800

    enhanced ui, added send to ewallet modal, added ledger balance api, added apiKey

--- src/app/api/request-payout/route.js
+++ src/app/api/request-payout/route.js
@@ -5,20 +5,36 @@ import { NextResponse } from 'next/server';
 
 export async function POST(req) {
   try {
-    // const { amount, description, email, typeOfBank, currency } = await req.json();
+    // const { firstName, middleName, lastName, amount, currency, description, procId, procDetail, email, mobileNo } = await req.json();
 
     const merchantId = process.env.NEXT_PUBLIC_DRAGONPAY_MERCHANT_ID;
     const password = process.env.DRAGONPAY_PASSWORD;
+    const apiKey = process.env.DRAGONPAY_APIKEY;
     const baseUrl = process.env.DRAGONPAY_PAYOUT_TEST_URL;
     const txnid = `txn${Date.now()}`;
+    const rundate = new Date.toISOString().split('T')[0];
 
     if (!merchantId || !password || !baseUrl) {
       throw new Error('Missing environment variables');
     }
 
+    // const payload = {
+    //   TxnId: txnid, 
+    //   FirstName: firstName,
+    //   MiddleName: middleName,
+    //   LastName: lastName,
+    //   Amount: amount,
+    //   Currency: currency,
+    //   Description: description,
+    //   ProcId: procId,
+    //   ProcDetail: procDetail,
+    //   RunDate: rundate,
+    //   Email: email,
+    //   MobileNo: mobileNo
+    // };
+
     const payload = {
-      TxnId: txnid,
-      FirstName: '',
+      TxnId: txnid, 
       FirstName: "Robertson",
       MiddleName: "Sy",
       LastName: "Chiang",
@@ -27,36 +43,36 @@ export async function POST(req) {
       Description: "Testing JSON payout",
       ProcId: "CEBL",
       ProcDetail: "dick@dragonpay.ph",
-      RunDate: "2019-02-26",
+      RunDate: rundate,
       Email: "dick@dragonpay.ph",
       MobileNo: "09175281679"
     };
 
     const endpoint = `${baseUrl}/${merchantId}/post`;
-    // const endpoint = `${baseUrl}/Transaction/Create`;
-    // const token = 'VFJDS1RJTkM6eGlhTngzbWcxTkxhZlB6';
-    // axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+    // const token = Buffer.from(`${merchantId}:${password}`, 'utf8').toString('base64');
+    
+    // axios.defaults.headers.common['Authorization'] = `Bearer ${apiKey}`;
     const response = await axios.post(endpoint, payload, {
       headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
       },
-      auth: {
-        username: merchantId,
-        password: password
-      }
+      // auth: {
+      //   username: merchantId,
+      //   password: password
+      // }
     })
     .then((res) => {
-      // console.log(res);
+      console.log("res:",res);
       return res;
     }).catch ((error)=> {
       console.log(error);
-      throw error;
+      throw error.response.data;
     });
 
     // console.log('Response from Dragonpay:', response);
-
-    return NextResponse.json(response.data);
+    return new NextResponse.json(response);
+    // return NextResponse.json(response);
 
     // if (response.data.Status === 'S' || response.data.Url) {
     //   return NextResponse.json({ url: response.data.Url });
@@ -66,6 +82,6 @@ export async function POST(req) {
 
   } catch (error) {
     console.error('Error processing payment:', error);
-    return NextResponse.json({ error: error.message }, { status: 500 });
+    return NextResponse.json({ error: error.Message }, { status: 500 });
   }
 }
--- src/app/wallet/page.tsx
+++ src/app/wallet/page.tsx
@@ -17,12 +17,21 @@ function classNames(...classes: any) {
   return classes.filter(Boolean).join(' ')
 }
 const Wallet = () => {
+    const [processors, setProcessors] = useState<PayoutProcessor[]>([]); 
+    const [filteredProcessors, setFilteredProcessors] = useState<PayoutProcessor[]>([]);  
+    const [selected, setSelected] = useState({
+        procId: 'NULL',
+        shortName: '',
+        logo: '',
+        status: '',
+        merchantFee: '',
+        userFee: ''    
+    }); 
+    const [sendType, setSendType] = useState('');
     const [openModal, setModal] = useState(false);
-    const handleModal = () => {
-      setModal(!openModal)
-    }
-    
-    const [formData, setFormData] = useState({
+    const walletIds = ['GCSH','PYMY','GRPY','BITC','TYME','TRMY','TNIK','SPAY','SMRT'];
+
+    const formFields = {
         firstName: '',
         middleName: '',
         lastName: '',
@@ -36,39 +45,58 @@ const Wallet = () => {
         mobileNo: '',
         birthDate:'',//(optional)
         nationality:'',//(optional)
-        address: {
+        address: {//(optional)
           street1: '',
           street2: '',
           barangay:'',
           city: '',
           province:'',
           country: ''
-        },//(optional)
-    });
-
-    const preSelectedBank = [
-    {
-        procId: 'NULL',
-        shortName: 'Select a Bank',
-        logo: '',
-        status: '',
-        merchantFee: '',
-        userFee: ''
-    }];
-
-    const [processors, setProcessors] = useState<PayoutProcessor[]>([]); 
-    const [selected, setSelected] = useState(preSelectedBank[0]); 
-  
-useEffect(() => {
-    const fetchProcessors = async () => {
-        // setLoadingProcessors(true);
-        const res = await fetch('/api/payout-processors');
-        const availableProcessors = await res.json(); 
-        // setLoadingProcessors(false);
-        setProcessors(availableProcessors);  
+        },
     };
-    fetchProcessors();
+    
+    const [formData, setFormData] = useState(formFields);
+    const [walletBalance, setWalletBalance] = useState(1000);
+
+    // console.log(selected);
+    useEffect(() => {
+        const fetchProcessors = async () => {
+            // setLoadingProcessors(true);
+            const res = await fetch('/api/payout-processors');
+            const availableProcessors = await res.json(); 
+            // setLoadingProcessors(false);
+            setProcessors(availableProcessors);  
+        };
+     fetchProcessors();
+ 
+    const fetchBalance = async () => {
+        // setLoadingProcessors(true);
+        const res = await fetch('/api/ledger-balance');
+        const balance = await res.json(); 
+        console.log(balance);
+        // setLoadingProcessors(false);
+        setWalletBalance(balance);  
+    };
+    // fetchBalance();
     }, []);
+
+    const handleModal = (type: string) => {
+        setSendType(type);
+        
+        if (type == 'bank') {
+            const bank = processors.filter((x)=> !(walletIds.includes(x.procId)));
+            setSelected({...selected, shortName: 'Select a Bank'});
+            setFilteredProcessors(bank);
+        }
+        else if (type == 'wallet') {
+            const wallet = processors.filter((x)=> (walletIds.includes(x.procId)));
+            setSelected({...selected, shortName: 'Select an e-wallet'});
+            setFilteredProcessors(wallet);
+        }
+        setFormData(formFields);// reset fields values
+        setModal(!openModal);
+      }
+      
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
@@ -87,29 +115,72 @@ useEffect(() => {
   
 
   return (
-    <div className='w-full h-full mx-auto lg:max-w-2xl flex flex-col'>
+    <div className='w-full h-full mx-auto lg:max-w-5xl flex flex-col'>
 
-        <div className='flex-1'>
+        <div className='flex-1 css-wallet-container'>
+        
         <section className="py-8">
             <Image src={logo} alt='logo' priority={true} width={350} className='m-auto block' />
         </section>
 
         <section className="pb-12">
-            <h1>Wallet</h1>
+            <h3 className='text-4xl font-bold'>Wallet</h3>
             
-            <button type="button"
-            onClick={handleModal} className="flex items-center justify-center text-base font-semibold h-10 rounded px-4 border transition outline-none !text-white bg-red-500 text-secondary border-secondary hover:brightness-110 hover:!bg-secondary focus:brightness-110 focus:ring-2 ring-secondary w-full">
-                Send to Bank Account
-            </button>
+            <div className="h-100 p-8 bg-white rounded-[20px] shadow-[0_15px_46px_rgba(0,0,0,0.05)]">
+                <div className="flex flex-col items-center h-full justify-between md:flex-row">
+                    <div className="flex flex-col justify-end">
+                        <div className="flex">
 
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5 mr-1.5">
+                            <path d="M1 4.25a3.733 3.733 0 0 1 2.25-.75h13.5c.844 0 1.623.279 2.25.75A2.25 2.25 0 0 0 16.75 2H3.25A2.25 2.25 0 0 0 1 4.25ZM1 7.25a3.733 3.733 0 0 1 2.25-.75h13.5c.844 0 1.623.279 2.25.75A2.25 2.25 0 0 0 16.75 5H3.25A2.25 2.25 0 0 0 1 7.25ZM7 8a1 1 0 0 1 1 1 2 2 0 1 0 4 0 1 1 0 0 1 1-1h3.75A2.25 2.25 0 0 1 19 10.25v5.5A2.25 2.25 0 0 1 16.75 18H3.25A2.25 2.25 0 0 1 1 15.75v-5.5A2.25 2.25 0 0 1 3.25 8H7Z" />
+                        </svg>
+
+                            <h4 className="text-sm font-bold uppercase">Balance</h4>
+
+                        </div>
+                        <div className="flex flex-col items-start md:flex-row md:items-end">
+                            <p className="text-4xl font-bold ">{walletBalance}</p>
+                        </div>
+                    </div>
+                    <div className="flex flex-wrap gap-5">
+
+                        {/* <button type="button" className="chakra-button css-1l7kpaq">
+                           Send to bank account</button>
+                        <button type="button" className="chakra-button css-1l7kpaq">
+                            Send to e-wallet</button> */}
+
+                        <button type="button"
+                        onClick={(e)=> handleModal('bank')} 
+                        className="sm:whitespace-nowrap	sm:w-auto inline-flex bg-red-500 appearance-none items-center justify-center select-none relative whitespace-pre-wrap align-middle outline-none w-full	leading-none rounded font-semibold transition duration-200 h-14 min-w-12 text-[15px] ps-6 pe-6 text-white cursor-pointer pt-2.5	pb-2.5	">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="size-4">
+                                <path fillRule="evenodd" d="M7.605 2.112a.75.75 0 0 1 .79 0l5.25 3.25A.75.75 0 0 1 13 6.707V12.5h.25a.75.75 0 0 1 0 1.5H2.75a.75.75 0 0 1 0-1.5H3V6.707a.75.75 0 0 1-.645-1.345l5.25-3.25ZM4.5 8.75a.75.75 0 0 1 1.5 0v3a.75.75 0 0 1-1.5 0v-3ZM8 8a.75.75 0 0 0-.75.75v3a.75.75 0 0 0 1.5 0v-3A.75.75 0 0 0 8 8Zm2 .75a.75.75 0 0 1 1.5 0v3a.75.75 0 0 1-1.5 0v-3ZM8 6a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z" clipRule="evenodd" />
+                            </svg>
+
+                            Send to Bank Account
+                        </button>
+
+                        <button type="button" 
+                        onClick={(e)=> handleModal('wallet')} 
+                        className="sm:whitespace-nowrap	sm:w-auto inline-flex bg-red-500 appearance-none items-center justify-center select-none relative whitespace-pre-wrap align-middle outline-none w-full	leading-none rounded font-semibold transition duration-200 h-14 min-w-12 text-[15px] ps-6 pe-6 text-white cursor-pointer pt-2.5	pb-2.5">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="size-4">
+                            <path d="M2 3.5A1.5 1.5 0 0 1 3.5 2h9A1.5 1.5 0 0 1 14 3.5v.401a2.986 2.986 0 0 0-1.5-.401h-9c-.546 0-1.059.146-1.5.401V3.5ZM3.5 5A1.5 1.5 0 0 0 2 6.5v.401A2.986 2.986 0 0 1 3.5 6.5h9c.546 0 1.059.146 1.5.401V6.5A1.5 1.5 0 0 0 12.5 5h-9ZM8 10a2 2 0 0 0 1.938-1.505c.068-.268.286-.495.562-.495h2A1.5 1.5 0 0 1 14 9.5v3a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 12.5v-3A1.5 1.5 0 0 1 3.5 8h2c.276 0 .494.227.562.495A2 2 0 0 0 8 10Z" />
+                        </svg>
+                            Send to Wallet
+                        </button>
+
+                    </div>
+                </div>
+            </div>
+           
+            
             {openModal &&
-            <div className='fixed top-0 left-0 w-full h-full bg-gray-900 bg-opacity-60 flex justify-center items-center'>
+            <div className='fixed top-0 left-0 w-full h-full bg-gray-500 bg-opacity-75 transition-opacity flex justify-center items-center'>
                 <div className='flex flex-col bg-white border shadow-sm rounded-xl pointer-events-auto dark:bg-neutral-800 dark:border-neutral-700 dark:shadow-neutral-700/70'>
                     <div className="flex justify-between items-center py-3 px-4 border-b dark:border-neutral-700">
                         <h3 className="font-bold text-red-500 dark:text-white">
-                            Send to Bank Account
+                            Send to {sendType == 'bank'?'Bank Account' :'E-wallet'}
                         </h3>
-                        <button type="button" className="text-red-500 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center" aria-label="Close" onClick={handleModal}>
+                        <button type="button" className="text-red-500 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center" aria-label="Close" onClick={(e)=> handleModal('')} >
                             <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                 <path className='clip-rule fill-rule'
                                     d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
@@ -156,7 +227,7 @@ useEffect(() => {
                         </div>
 
                         <div className='mb-3'>
-                            <label className="uppercase mb-4 text-gray-800 text-sm">Bank Name:</label>
+                            <label className="uppercase mb-4 text-gray-800 text-sm">{sendType == 'bank'? 'Bank Name': 'E-wallet'}</label>
                             <Listbox value={selected} onChange={setSelected}>
                             {({ open }) => (
                                 <>
@@ -175,7 +246,7 @@ useEffect(() => {
                                     transition
                                     className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none data-[closed]:data-[leave]:opacity-0 data-[leave]:transition data-[leave]:duration-100 data-[leave]:ease-in sm:text-sm"
                                     >
-                                    {processors.filter((x)=> !(['GCSH','PYMY','GRPY','BITC','TYME','TRMY','TNIK','SPAY','SMRT'].includes(x.procId))).map((item, index) => (
+                                    {filteredProcessors.map((item, index) => (
                                         <ListboxOption
                                         key={index}
                                         className={({ focus }) =>
@@ -222,10 +293,6 @@ useEffect(() => {
                 </div>
             </div>
             }
-
-            <button type="button" className="flex items-center justify-center text-base font-semibold h-10 rounded px-4 border transition outline-none !text-white bg-red-500 text-secondary border-secondary hover:brightness-110 hover:!bg-secondary focus:brightness-110 focus:ring-2 ring-secondary w-full">
-                Send to Wallet
-            </button>
         </section>
         </div>
     </div>
